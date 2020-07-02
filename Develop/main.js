3cc942e9d916e129f2e5c0eff9f6709bc3cfd29f
Andrew Dalba <andrewdalba@gmail.com>
2020-07-02 15:10:23 -0400

    create timeblocks and begin writing js

---
+++ Develop/main.js
@@ -0,0 +1,36 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// targets the whole document -- "ready()" calls a function as- 
+// soon as the pages DOM becomes safe to manipulate
+$(document).ready(function(){
+
+})
+// THEN the current day is displayed at the top of the calendar
+// moments.js
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// html
+// maybe just to 9-5
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// change the class of the element in js depending on what moment.js is
+
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// can be done in the html using <textarea>
+
+
+// WHEN I click the save button for that timeblock
+// make sure the Save button works
+// THEN the text for that event is saved in local storage
+// do we need j.son stringify??
+
+// WHEN I refresh the page
+// THEN the saved events persist
+// Event.preventDefault(event);
+
